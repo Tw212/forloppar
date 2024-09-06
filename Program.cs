3420c97b43b39a47be87e7356890d553cea6fbbe
Tw212 <Tewe07001@utb.vaxjo.se>
2024-09-06 13:59:23 +0200

    forloppar

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*int räknare;
+for(räknare=0;räknare<=20;räknare++)
+{
+    Console.WriteLine(räknare+" ");
+}*/
+
+using System.Security.Cryptography.X509Certificates;
+
+int räknare;
+int tal = int.Parse(Console.ReadLine());
+for(räknare = 0 ;räknare<=101;räknare++)
+{
+    Console.WriteLine(räknare+tal);
+   
+}
